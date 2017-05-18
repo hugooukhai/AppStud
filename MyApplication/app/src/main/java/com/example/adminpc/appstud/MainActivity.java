1c1d3696918df0b0ba366edea2d52d9fdcaa57ea
Hugo Oukhai <hugo.oukhai@gmx.fr>
2017-05-18 13:28:40 +0200

    Add permission request

--- MyApplication/app/src/main/java/com/example/adminpc/appstud/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/adminpc/appstud/MainActivity.java
@@ -1,18 +1,34 @@
 package com.example.adminpc.appstud;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.MenuItem;
 
-public class MainActivity extends FragmentActivity {
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks {
 
 
-   private ViewPager mPager;
-   private ViewPagerAdapter mPagerAdapter;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private ViewPager mPager;
+    private ViewPagerAdapter mPagerAdapter;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private MapFragment mMapFragment;
+    private ListFragment mListFragment;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -38,6 +54,8 @@ public class MainActivity extends FragmentActivity {
 
     };
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,12 +67,79 @@ public class MainActivity extends FragmentActivity {
         mPager = (ViewPager) findViewById(R.id.viewPager);
         mPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         // Add fragment to the pager, order matters for menu binding
-        mPagerAdapter.addFragment(new MapFragment());
-        mPagerAdapter.addFragment(new ListFragment());
+        mMapFragment = new MapFragment();
+        mListFragment = new ListFragment();
+        mPagerAdapter.addFragment(mMapFragment);
+        mPagerAdapter.addFragment(mListFragment);
         mPager.setAdapter(mPagerAdapter);
+
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
 
 
     }
 
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
 
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+       //check permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("Permission ","User didn't accepted geoloc yet, ask for it");
+
+            // Ask for permissions
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+        }
+        else {
+            //we got permission, get last location
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                    mGoogleApiClient);
+            if (mLastLocation != null) {
+                //we got last location, show it on map
+               Log.d("latitude ", ""+ mLastLocation.getLatitude());
+               Log.d("longitude ", ""+ mLastLocation.getLongitude());
+
+            }else{
+                //we don't have location, ask for update LocationRequest locationRequest = new LocationRequest();
+                LocationRequest locationRequest = new LocationRequest();
+                locationRequest.setNumUpdates(1);
+                locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                Log.d("Launch","Location request");
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        mLastLocation = location;
+                        Log.d("latitude ", " on loc changed "+ mLastLocation.getLatitude());
+                        Log.d("longitude ", " on loc changed "+ mLastLocation.getLongitude());
+                    }
+                });
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mGoogleApiClient.unregisterConnectionCallbacks(this);
+    }
 }
